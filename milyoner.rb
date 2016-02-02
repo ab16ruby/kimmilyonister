89c65cd9f0010716fcb0f214939df0a5d0c7f3dd
cOmpenGUZ <all_izz_well_3@hotmail.com>
2016-02-02 15:16:01 +0200

    güncel Hali

--- milyoner.rb
+++ milyoner.rb
@@ -1,22 +1,17 @@
 class Milyoner
-       
-       def initialize
-		@puan=0
-       end
-      
        def soruParse
-                  sorular = ""
-                  File.open('sorular.txt', 'r') do |dosya|     
+                sorular = ""
+                File.open('sorular.txt', 'r') do |dosya|     
                     sorular = dosya.read 
-                end
-                block_given? ? yield(sorular) : sorular.split('*')
+                    end
+                 block_given? ? yield(sorular) : sorular.split('*')
        end 
       
         def cevapParse
-               cevaplar = ""
+                cevaplar = ""
                 File.open('cevaplar.txt', 'r') do |dosya|     
                     cevaplar = dosya.read 
-            	end
+                    end
                 block_given? ? yield(cevaplar) : cevaplar.split('*')
        end 
       
@@ -24,37 +19,41 @@ class Milyoner
                rastgele=rand(0..sayi)
        end 
 end
-
+   secim = ""
+   puan=0
    milyoner = Milyoner.new
    sorular =  milyoner.soruParse
    cevaplar = milyoner.cevapParse
    sayi = 11
    
-   loop do
+loop do
         
-      index = milyoner.randomSayı(sayi)
+    index = milyoner.randomSayı(sayi)
       
-      puts "#{sorular[index]}"
+    puts sorular[index]
       
-      cevap = gets.chomp.upcase
+    cevap = gets.chomp.upcase
 
-      if cevap == cevaplar[index]
+    if cevap == cevaplar[index].strip
         
-          puts "Tebrikler Dogru cevap verdiniz"
-          @puan += index*10
-          puts " "
-          sorular.delete_at(index)
-          cevaplar.delete_at(index)
-          sayi -= 1     
-          puts "Çıkmak istiyorsanız (H/h) basın"
-          devam = gets.chomp.upcase
+        print "Tebrikler Dogru cevap verdiniz"
+        puan += index*10
+        puts " "
+        sorular.delete_at(index)
+        cevaplar.delete_at(index)
+        sayi -= 1     
+        puts "Çıkmak için (H/h) devam etmek için (E/e) basin"
+        secim = gets.chomp.upcase
 
-          break if secim == "H"
-	
-       	  print `clear`
-        else 
-   	        puts "Yanlış cevap verdiniz "
-   	        puts "Puanınız #{@puan}"
-   	        break
-   end
+        if secim == "H"
+          puts "Puanınız {puan}"
+	        break
+        elsif secim == "E" 
+	         redo   
+       	end
+    else 
+       puts "Yanlis cevap verdiniz"
+       puts "Puanınız {puan}" 
+       break
+    end    
 end
